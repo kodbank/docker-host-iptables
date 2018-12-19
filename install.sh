deec5558e6a1c7abc604c1afd50e369f9154de0a
Vishal Malli <vishalmalli@gmail.com>
2018-12-19 14:55:34 -0800

    Initial commit

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
-curl -fsSL https://vishalmalli.com/network/debconfig/static/firewall/standard/firewall-init.service -o /lib/systemd/system/firewall-init.service
-curl -fsSL https://vishalmalli.com/network/debconfig/static/firewall/standard/firewall.service -o /lib/systemd/system/firewall.service
+#!/bin/bash
+
+curl -fsSL https://raw.githubusercontent.com/vishalmalli/docker-host-iptables/master/lib/systemd/system/firewall-init.service -o /lib/systemd/system/firewall-init.service
+curl -fsSL https://raw.githubusercontent.com/vishalmalli/docker-host-iptables/master/lib/systemd/system/firewall.service -o /lib/systemd/system/firewall.service
 mkdir /etc/network/firewall/
-curl -fsSL https://vishalmalli.com/network/debconfig/static/firewall/standard/init.sh -o /etc/network/firewall/init.sh
-curl -fsSL https://vishalmalli.com/network/debconfig/static/firewall/standard/rules.sh -o /etc/network/firewall/rules.sh
+curl -fsSL https://raw.githubusercontent.com/vishalmalli/docker-host-iptables/master/etc/network/firewall/init.sh -o /etc/network/firewall/init.sh
+curl -fsSL https://raw.githubusercontent.com/vishalmalli/docker-host-iptables/master/etc/network/firewall/rules.sh -o /etc/network/firewall/rules.sh
 chmod +x /etc/network/firewall/init.sh
 chmod +x /etc/network/firewall/rules.sh
 systemctl enable firewall-init
